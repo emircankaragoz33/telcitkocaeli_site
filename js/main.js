7f4319e0aa24d23f2349dfb6635f6dbcaa121bba
Emircan <Emircan@DESKTOP-3E70GC6>
2026-04-03 14:22:22 +0300

    Mail adreslerini info@guclucit.com olarak geri guncelle

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@
             '<div class="top-contact-bar">' +
             '<marquee behavior="scroll" direction="left" scrollamount="5">' +
             '<i class="fas fa-phone-alt me-1"></i> 0 542 415 95 62 &nbsp;&nbsp;&nbsp;&bull;&nbsp;&nbsp;&nbsp; ' +
-            '<i class="fas fa-envelope me-1"></i> guclucit@gmail.com' +
+            '<i class="fas fa-envelope me-1"></i> info@guclucit.com' +
             '</marquee>' +
             '</div>';
 
